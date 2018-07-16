36a3de6bd8c5626aca3b97f0a31518f312b7c198
Debby Wasserstein <debby.was@gmail.com>
2018-07-16 16:13:08 -0400

    added function for clock - starts on click - not connected to deck yet

--- js/app.js
+++ js/app.js
@@ -1,78 +1,8 @@
-//stopwatch by Ryan Waite
-
-
-const StopWatch = function StopWatch() {
-  const self = this;
-
-  let hours = 0;
-  let minutes = 0;
-  let seconds = 0;
-
-  let timer;
-  let on = false;
-
-  self.startTimer = function(callback) {
-    if(on === true) { return; }
-    on = true;
-    timer = setInterval(function(){
-      seconds++;
-      if(seconds === 60) {
-        seconds = 0;
-        minutes++;
-        if(minutes === 60) {
-          minutes = 0;
-          hours++;
-        }
-      }
-      if(callback && callback.constructor === Function) {
-        callback();
-      }
-    }, 1000);
-    console.log('timer started');
-  }
-
-  self.stopTimer = function() {
-    clearInterval(timer);
-    on = false;
-    console.log('timer ended: ', self.getTimeString());
-  }
-
-  self.resetTimer = function() {
-    self.stopTimer();
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-  }
-
-  self.getTimeString = function() {
-    let hour = hours > 9 ? String(hours) : '0' + String(hours);
-    let minute = minutes > 9 ? String(minutes) : '0' + String(minutes);
-    let second = seconds > 9 ? String(seconds) : '0' + String(seconds);
-    let timeString = hour + ':' + minute + ':' + second;
-    return timeString;
-  }
-
-  self.getHours = function() {
-    return hours;
-  }
-
-  self.getMinutes = function() {
-    return minutes;
-  }
-
-  self.getSeconds = function() {
-    return seconds;
-  }
-}
-let watch = new StopWatch();
-watch.stopTimer(function() {
-  console.log(watch.getTimeString());
-})
-
 
 
 let deck = document.querySelector(".deck");
 let allCards = deck.querySelectorAll("li.card")
+let clockOff = true;
 
 
 //shuffeling cards - input
@@ -88,6 +18,10 @@ shuffleDeck();
 //adds event listener when clicked
 allCards.forEach(function(card){
   card.addEventListener("click", function(){
+    if (clockOff) {
+      startClock();
+      clockOff = false;
+    }
       if (!card.classList.contains("match") && openCards.length < 2 && !openCards.includes(card)) {
         toggle(card);
         storeCards(card);
@@ -169,10 +103,16 @@ function stars(){
         star.style.display = "none";
         break;
     }
-
  }}
 
-
+//clock
+function startClock(){
+  time = 0;
+  let clockId = setInterval(function(){
+    time++;
+    console.log(time);
+  }, 1000);
+}
 
 
 
