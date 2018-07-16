5d3ed79bc17e8dc8c05c40c6eaa73b20b126c277
Debby Wasserstein <debby.was@gmail.com>
2018-07-16 10:26:31 -0400

    added Ryan Wait's stopwatch as clock for game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,76 @@
+//stopwatch by Ryan Waite
+
+
+const StopWatch = function StopWatch() {
+  const self = this;
+
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+
+  let timer;
+  let on = false;
+
+  self.startTimer = function(callback) {
+    if(on === true) { return; }
+    on = true;
+    timer = setInterval(function(){
+      seconds++;
+      if(seconds === 60) {
+        seconds = 0;
+        minutes++;
+        if(minutes === 60) {
+          minutes = 0;
+          hours++;
+        }
+      }
+      if(callback && callback.constructor === Function) {
+        callback();
+      }
+    }, 1000);
+    console.log('timer started');
+  }
+
+  self.stopTimer = function() {
+    clearInterval(timer);
+    on = false;
+    console.log('timer ended: ', self.getTimeString());
+  }
+
+  self.resetTimer = function() {
+    self.stopTimer();
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+  }
+
+  self.getTimeString = function() {
+    let hour = hours > 9 ? String(hours) : '0' + String(hours);
+    let minute = minutes > 9 ? String(minutes) : '0' + String(minutes);
+    let second = seconds > 9 ? String(seconds) : '0' + String(seconds);
+    let timeString = hour + ':' + minute + ':' + second;
+    return timeString;
+  }
+
+  self.getHours = function() {
+    return hours;
+  }
+
+  self.getMinutes = function() {
+    return minutes;
+  }
+
+  self.getSeconds = function() {
+    return seconds;
+  }
+}
+let watch = new StopWatch();
+watch.stopTimer(function() {
+  console.log(watch.getTimeString());
+})
+
+
+
 let deck = document.querySelector(".deck");
 let allCards = deck.querySelectorAll("li.card")
 
@@ -15,15 +88,15 @@ shuffleDeck();
 //adds event listener when clicked
 allCards.forEach(function(card){
   card.addEventListener("click", function(){
-    if (!card.classList.contains("match") && openCards.length < 2 && !openCards.includes(card)) {
-      toggle(card);
-      storeCards(card);
-        if (openCards.length === 2) {
-          match(card);
-          addMoves();
-          checkMoves();
-        }
-    }
+      if (!card.classList.contains("match") && openCards.length < 2 && !openCards.includes(card)) {
+        toggle(card);
+        storeCards(card);
+          if (openCards.length === 2) {
+            match(card);
+            addMoves();
+            checkMoves();
+          }
+      }
   })
 })
 
@@ -103,9 +176,12 @@ function stars(){
 
 
 
-
-
-
+//game over - win
+// function winner(){
+//   if(openCards.length === openCards.length){
+//     alert("You won!");
+//   }
+// }
 
 
 /*
